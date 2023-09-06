bdc5b60587da22fac34fb6726ab5f07fa1a7f5c8
David Chen <chendavid383@gmail.com>
2023-09-05 20:33:47 -0700

    removing fetches

--- app/projects/(tft)/shred-table/page.js
+++ app/projects/(tft)/shred-table/page.js
@@ -3,14 +3,6 @@
 import { useEffect } from "react"
 
 export default function ShredTable() {
-  useEffect(() => {
-    ;(async () => {
-      console.log("fewf")
-      const res = await fetch("/api/tft")
-      const data = await res.json();
-      console.log(data)
-    })()
-  }, [])
   return (
     <table>
       <thead>
