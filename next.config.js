b6d1aaf017144562490c733cf15c9bc7f2b94aed
David Chen <chendavid383@gmail.com>
2023-09-07 02:46:27 -0700

    adding config back

--- next.config.js
+++ next.config.js
@@ -1,7 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
+  /**
+   * Enable static exports for the App Router.
+   *
+   * @see https://nextjs.org/docs/pages/building-your-application/deploying/static-exports
+   */
+  output: "export",
+
+  /**
+   * Disable server-based image optimization. Next.js does not support
+   * dynamic features with static exports.
+   *
+   * @see https://nextjs.org/docs/pages/api-reference/components/image#unoptimized
+   */
+  images: {
+    unoptimized: true,
   },
 }
 
