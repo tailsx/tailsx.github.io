b01cf0bec877ef714bc12b6f8f381e1d5488b225
David Chen <chendavid383@gmail.com>
2023-09-09 04:06:39 -0700

    adding update to hero

--- app/page.js
+++ app/page.js
@@ -8,6 +8,9 @@ import Headshot from "./components/headshot"
 import Image from "next/image"
 import DavidCards from "./components/davidCards"
 import DavidFeatures from "./components/davidFeatures"
+import { Playfair_Display } from "next/font/google"
+
+const font = Playfair_Display({ subsets: ["latin"], style: ["normal"], weight: ["700"] })
 
 export default function Home() {
   return (
@@ -36,9 +39,16 @@ export default function Home() {
 function Hero() {
   return (
     <div className="flex flex-col md:flex-row gap-2 justify-center items-center relative overflow-hidden">
-      <div className="flex flex-col text-center justify-center z-10 min-h-[80vh]">
-        <h1 className="text-4xl md:text-6xl font-extrabold text-primary">David Chen</h1>
-        <span className="text-xl md:text-2xl text-neutral">Generic Name. Unique Individual.</span>
+      <div className="flex flex-col justify-center z-10 min-h-[80vh]">
+        <span className="text-base md:text-lg lg:text-2xl text-neutral">Generic Name.</span>
+        <span
+          className={`${font.className} text-5xl md:text-6xl lg:text-[72px] uppercase font-bold bg-clip-text text-transparent animate-uniqueTitle`}
+        >
+          Unique
+        </span>
+        <span className="px-1 text-3xl md:text-4xl lg:text-5xl text-primary tracking-[6px] md:tracking-[8px] lg:tracking-[8px]">
+          Individual
+        </span>
       </div>
       <Image
         style={{ objectFit: "cover", opacity: "60%" }}
