793714a4d107abd3dc13ed40b2fabf2665d8d529
David Chen <chendavid383@gmail.com>
2023-09-13 21:44:24 -0700

    removing more

--- app/projects/mahjong/games/[game]/page.js
+++ app/projects/mahjong/games/[game]/page.js
@@ -2,11 +2,13 @@ import { fetchGameData } from "@/app/api/utils/mahjong"
 import Table from "@/app/components/shared/table"
 
 export default async function MahjongGame(props) {
-  const data = await fetchGameData(decodeURI(props.params.game))
+ /*  const data = await fetchGameData(decodeURI(props.params.game))
 
   return (
     <main className="">
       <Table headers={data.players} data={data.rounds} />
     </main>
-  )
+  ) */
+
+  return <main>to be continued</main>
 }
